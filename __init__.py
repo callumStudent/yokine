db5ceb1739ed182b5c380fd182fbb3d8b9eeda96
Callum Ingley <callum.ingley@gmail.com>
2016-02-25 16:23:54 +0800

    First commit. Basic structure, homepage and News

---
+++ __init__.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, url_for
+import os
+
+app = Flask(__name__)
+app.debug = True
+#app.config.from_object('settings')
+#db - SQLAlchemy(app)
+
+#Migrations
+#migrate = Migrate(app, db)
+
+#Blueprints
+#from yokine-archery.folder.views import blueprint_name
+#app.register_blueprint(blueprint_name)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/news')
+def news():
+    return render_template('news.html')
+    
+if __name__ == "__main__":
+    port = int(os.getenv('PORT','8080'))
+    host = os.getenv('IP', '0.0.0.0')
+    app.run(port=port, host=host)
