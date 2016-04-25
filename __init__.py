fda425570e329b8397c8d3ac064e6ad308bfa582
Callum Ingley <callum.ingley@gmail.com>
2016-04-25 07:37:14 +0000

    updated news page to include media, added map to location

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, request, render_template, url_for
 import os
 
 app = Flask(__name__)
@@ -20,24 +20,24 @@ def index():
 @app.route('/news')
 def news():
     return render_template('news.html')
-    
-@app.route('/media')
-def media():
-    return render_template('media.html')
-    
+
+@app.route('/gallery')
+def gallery():
+    return render_template('gallery.html')
+
 @app.route('/location')
 def location():
     return render_template('location.html')
-    
+
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-    
+
 @app.route('/submit', methods=['POST'])
 def submit():
     if(request.method == 'POST'):
         return render_template()
-    
+
 if __name__ == "__main__":
     port = int(os.getenv('PORT','8080'))
     host = os.getenv('IP', '0.0.0.0')
